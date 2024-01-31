fc2b30103e66aaab53a06dffb4da44af04a37c1f
AdrianAguinaga <adrian.aguinaga@uabc.edu.mx>
2024-01-30 20:00:30 -0800

    107-S3

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Footer from './components/footer';
+import Navbar from './components/navbar';
+import Catalog from './pages/catalog';
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/js/bootstrap.min.js"
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        {/* this is only for the sake of having something that change */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Navbar></Navbar>
+      <Catalog></Catalog>
+      <Footer></Footer>
+      
+
+     
+
+
     </div>
   );
 }
---
+++ src/components/quantityPicker.jsx
@@ -0,0 +1,38 @@
+import "./quantityPicker.css";
+import { useState } from "react";
+
+
+function QuantityPicker()
+{
+
+    const[quantity,setQuantity] = useState (1);
+   // quantity = 10; this is what we dont want to do
+
+
+    function decrease(){
+        if(quantity===1) return;       
+        let value = quantity - 1;
+        setQuantity(value)
+        console.log("Decreasing");
+    }
+    //create the increase function
+
+    //please use the same logic but for the increasing.
+    function increase(){
+        let value = quantity + 1;
+        setQuantity(value);
+        console.log("Increasing");
+        //small change`
+    }
+
+
+    return (
+        <div className="quantityPicker">
+        <button class="btn btn-primary" disabled = {quantity===1}  onClick={decrease} >-</button>
+        <label>{quantity}</label>
+        <button class="btn btn-primary" onClick={increase}>+</button>
+        </div>
+    );
+}
+
+export default QuantityPicker;
